b649240c6e04e45e2f971ef3b98d281a23a55352
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-06-22 08:03:36 -0700

    Add project files.

---
+++ BotmonClass.cs
@@ -0,0 +1,178 @@
+﻿using Comfort.Common;
+using EFT;
+using EFT.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Reflection;
+using CWX_DebuggingTool.Models;
+
+namespace CWX_DebuggingTool
+{
+    public sealed class BotmonClass : MonoBehaviour, IDisposable
+    {
+        private static BotmonClass _instance;
+
+        private GUIContent _guiContent;
+        private GUIStyle _textStyle;
+        private Player _player;
+        private Dictionary<string, List<Player>> _zoneAndPlayers = new Dictionary<string, List<Player>>();
+        private Dictionary<string, BotRoleAndDiffClass> _playerRoleAndDiff = new Dictionary<string, BotRoleAndDiffClass>();
+        private List<BotZone> _zones;
+        private GameWorld _gameWorld;
+        private IBotGame _botGame;
+        private Rect _rect;
+        private string _content = "";
+        private Vector2 _guiSize;
+        private float _distance;
+
+        private BotmonClass()
+        {
+
+        }
+
+        public int Mode { get; set; }
+
+        public static BotmonClass Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new BotmonClass();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void Dispose()
+        {
+            var gameWorld = Singleton<GameWorld>.Instance;
+
+            var gameobj = gameWorld.GetComponent<BotmonClass>();
+            Destroy(gameobj);
+            _instance = null;
+            GC.SuppressFinalize(this);
+        }
+
+        ~BotmonClass()
+        {
+            ConsoleScreen.Log("BotMonitor Disabled on game end");
+        }
+
+        public void Awake()
+        {
+            // Get GameWorld Instance
+            _gameWorld = Singleton<GameWorld>.Instance;
+
+            // Get BotGame Instance
+            _botGame = Singleton<IBotGame>.Instance;
+
+            // Get Player from GameWorld
+            _player = _gameWorld.MainPlayer;
+
+            // Make new rect to use for GUI
+            _rect = new Rect(0, 60, 0, 0);
+
+            // Get all BotZones
+            _zones = LocationScene.GetAllObjects<BotZone>().ToList();
+
+            // Set up the Dictionary
+            foreach (var botZone in _zones)
+            {
+                _zoneAndPlayers.Add(botZone.name, new List<Player>());
+            }
+
+            // Add existing Bots to list
+            if (_gameWorld.AllPlayers.Count > 1)
+            {
+                foreach (var player in _gameWorld.AllPlayers)
+                {
+                    if (player.IsYourPlayer) continue;
+
+                    _playerRoleAndDiff.Add(player.ProfileId, GetBotRoleAndDiffClass(player.Profile.Info));
+
+                    var theirZone = player.AIData.BotOwner.BotsGroup.BotZone.NameZone;
+
+                    _zoneAndPlayers[theirZone].Add(player);
+                }
+            }
+
+            // Sub to Event to get and add Bot when they spawn
+            _botGame.BotsController.BotSpawner.OnBotCreated += owner =>
+            {
+                var player = owner.GetPlayer;
+                _zoneAndPlayers[player.AIData.BotOwner.BotsGroup.BotZone.NameZone].Add(player);
+                _playerRoleAndDiff.Add(player.ProfileId, GetBotRoleAndDiffClass(player.Profile.Info));
+            };
+        }
+
+        public BotRoleAndDiffClass GetBotRoleAndDiffClass(InfoClass info)
+        {
+            var settings = info.GetType().GetField("Settings", BindingFlags.Public | BindingFlags.Instance).GetValue(info);
+
+            var role = settings.GetType().GetField("Role", BindingFlags.Instance | BindingFlags.Public).GetValue(settings).ToString();
+            var diff = settings.GetType().GetField("BotDifficulty", BindingFlags.Instance | BindingFlags.Public).GetValue(settings).ToString();
+
+            return new BotRoleAndDiffClass(string.IsNullOrEmpty(role) ? "" : role, string.IsNullOrEmpty(diff) ? "" : diff);
+        }
+
+        public void OnGUI()
+        {
+            // set basics on GUI
+            if (_textStyle == null)
+            {
+                _textStyle = new GUIStyle(GUI.skin.box);
+                _textStyle.alignment = TextAnchor.MiddleLeft;
+                _textStyle.fontSize = 20;
+                _textStyle.margin = new RectOffset(3, 3, 3, 3);
+            }
+
+            // new GUI Content
+            if (_guiContent == null)
+            {
+                _guiContent = new GUIContent();
+            }
+
+            // If Mode Greater than or equal to 1 show total
+            if (Mode >= 1)
+            {
+                _content = string.Empty;
+                _content += $"Total = {_gameWorld.AllPlayers.Count - 1}\n";
+            }
+
+            // If Mode Greater than or equal to 2 show total for each zone
+            if (Mode >= 2 && _zoneAndPlayers != null)
+            {
+                foreach (var zone in _zoneAndPlayers)
+                {
+                    if (_zoneAndPlayers[zone.Key].FindAll(x => x.HealthController.IsAlive).Count <= 0) continue;
+
+                    _content += $"{zone.Key} = {_zoneAndPlayers[zone.Key].FindAll(x => x.HealthController.IsAlive).Count}\n";
+
+                    // If Mode Greater than or equal to 3 show Bots individually also
+                    if (Mode < 3) continue;
+
+                    foreach (var player in _zoneAndPlayers[zone.Key].Where(player => player.HealthController.IsAlive))
+                    {
+                        _distance = Vector3.Distance(player.Position, _player.Position);
+                        _content += $"> [{_distance:n2}m] [{_playerRoleAndDiff.First(x => x.Key == player.ProfileId).Value.Role}] " +
+                                    $"[{player.Profile.Side}] [{_playerRoleAndDiff.First(x => x.Key == player.ProfileId).Value.Difficulty}] {player.Profile.Nickname}\n";
+                    }
+                }
+            }
+
+            _guiContent.text = _content;
+
+            _guiSize = _textStyle.CalcSize(_guiContent);
+
+            _rect.x = Screen.width - _guiSize.x - 5f;
+            _rect.width = _guiSize.x;
+            _rect.height = _guiSize.y;
+
+            GUI.Box(_rect, _guiContent, _textStyle);
+        }
+    }
+}
---
+++ DebuggingTool.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using Comfort.Common;
+using EFT;
+using EFT.Console.Core;
+using EFT.UI;
+
+namespace CWX_DebuggingTool
+{
+    [BepInPlugin("com.cwx.debuggingtool", "cwx-debuggingtool", "1.1.0")]
+    public class DebuggingTool : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            ConsoleScreen.Processor.RegisterCommandGroup<DebuggingTool>();
+        }
+
+        [ConsoleCommand("BotMonitor")]
+        public static void BotMonitorConsoleCommand([ConsoleArgument("", "Options: 0 = off, 1 = Total bots, 2 = 1+Total bots per Zone, 3 = 2+Each bot")] int value )
+        {
+            switch (value)
+            {
+                case 0:
+                    DisableBotMonitor();
+                    ConsoleScreen.Log("BotMonitor disabled");
+                    break;
+                case 1:
+                    EnableBotMonitor(1);
+                    ConsoleScreen.Log("BotMonitor enabled with only Total");
+                    break;
+                case 2:
+                    EnableBotMonitor(2);
+                    ConsoleScreen.Log("BotMonitor enabled with Total and per zone Total");
+                    break;
+                case 3:
+                    EnableBotMonitor(3);
+                    ConsoleScreen.Log("BotMonitor enabled with Total, per zone Total and each bot");
+                    break;
+                default:
+                    // fail to load, or wrong option used
+                    ConsoleScreen.LogError("Wrong Option used, please use 0, 1, 2 or 3");
+                    break;
+            }
+        }
+
+        public static void DisableBotMonitor()
+        {
+            BotmonClass.Instance.Dispose();
+        }
+
+        public static void EnableBotMonitor(int option)
+        {
+            var gameWorld = Singleton<GameWorld>.Instance;
+
+            var btmInstance = gameWorld.gameObject.AddComponent<BotmonClass>();
+
+            btmInstance.Mode = option;
+        }
+    }
+}
