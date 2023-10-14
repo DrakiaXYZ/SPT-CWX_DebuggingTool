0f10739196c5acea579b6795c127644ba34a2078
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-10-14 12:53:17 -0700

    Update for 3.7.1 / 26535

--- DebuggingTool.cs
+++ DebuggingTool.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 
 namespace CWX_DebuggingTool
 {
-    [BepInPlugin("com.cwx.debuggingtool-dxyz", "cwx-debuggingtool-dxyz", "2.2.0")]
+    [BepInPlugin("com.cwx.debuggingtool-dxyz", "cwx-debuggingtool-dxyz", "2.2.1")]
     public class DebuggingTool : BaseUnityPlugin
     {
         public static ConfigEntry<BotMonitorMode> DefaultMode;
